3e057df0b158d204a6527249494a115a9afbbcd1
socket772 <socket772@protonmail.com>
2025-03-11 12:02:04 +0100

    Cambiata libreria per il confronto

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 #![allow(clippy::needless_return)]
+use dssim::DssimImage;
+use image;
+use image_compare;
 use std::{
     env::args,
+    fs::File,
+    io::BufReader,
     sync::{Arc, Mutex},
     thread, vec,
 };
-
-use dssim::DssimImage;
 use walkdir::WalkDir;
 
 struct Immagini {
     vettore_immagini: Vec<String>,
     posizione: usize,
-    base_image: DssimImage<f32>,
+    base_image: image::ImageReader<BufReader<File>>,
 }
 
 fn main() {
@@ -20,8 +23,6 @@ fn main() {
     if args.len() != 3 {
         return;
     }
-
-    let dssim = dssim::new();
 
     let lista_files = WalkDir::new(args[1].clone());
     let mut vettore: Vec<String> = vec![];
@@ -35,7 +36,7 @@ fn main() {
     let dati_condivisi = Immagini {
         vettore_immagini: vettore,
         posizione: 0,
-        base_image: dssim::load_image(&dssim, args[2].clone()).unwrap(),
+        base_image: image::ImageReader::open(args[2].clone()).unwrap(),
     };
 
     let numero_immagini = dati_condivisi.vettore_immagini.len();
@@ -78,7 +79,7 @@ fn thread_operation(mutex_lock: Arc<Mutex<Immagini>>, numero_immagini: usize) ->
     // Copio le variabili che mi servono dalla struct
     let posizione_temp = dati_condivisi.posizione;
     let vettore_immagini_temp = dati_condivisi.vettore_immagini.clone();
-    let base_image = dati_condivisi.base_image.clone();
+    let base_image = dati_condivisi.base_image;
 
     // Controllo se ci sono altre canzoni da convertire
     if posizione_temp >= numero_immagini {
@@ -92,11 +93,11 @@ fn thread_operation(mutex_lock: Arc<Mutex<Immagini>>, numero_immagini: usize) ->
     // Estraggo il nome della canzione
     let nome_immagine = vettore_immagini_temp[posizione_temp].clone();
     // println!("{}", nome_immagine);
-    let immagine_target = dssim::load_image(&dssim, nome_immagine.clone()).unwrap();
+    let immagine_target = image::ImageReader::open(nome_immagine.clone()).unwrap();
 
-    let result = dssim.compare(&base_image, &immagine_target).0;
+    let result = image_compare::rgb_hybrid_compare(&base_image, &immagine_target).unwrap();
 
-    println!("{};{}", result, nome_immagine);
+    println!("{:?};{}", result, nome_immagine);
 
     return 0;
 }
